78b0245cf9e4efc7d23eabab26f29d20518ea921
Mr. Hasan <hasanshahriar32@gmail.com>
2023-05-09 12:42:08 +0600

    updated service model

--- Model/serviceModel.js
+++ Model/serviceModel.js
@@ -1,13 +1,12 @@
 const { Schema, model } = require("mongoose");
 const serviceSchema = new Schema({
-  lugTitle : String,
-developerinfo : String,  
-serviceImage : String,
-aboutService : String,
-serviceDetails : String,
-technology : String,
-features : String,
-category : String,
+  slugTitle: String,
+  developerInfo: Object,
+  serviceImage: Object,
+  aboutService: String,
+  serviceDetails: String,
+  technology: Array,
+  features: Array,
+  categoryId: String,
 });
 module.exports = model("service", serviceSchema);
-
